054c1b918fbd21b83217db483006b572392f1cba
alex hernandez <alexanderohernandez21@gmail.com>
2022-05-25 11:43:54 -0700

    added sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,1 +1,31 @@
 // TODO: Create a service worker that caches static assets:
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
+import { precacheAndRoute } from 'workbox-precaching';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+
+// Import the expiration plugin
+import { ExpirationPlugin } from 'workbox-expiration';
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const cachName = 'static-resources';
+const matchCallback = ({request})=> {
+    console.log(request);
+    return(
+        request.destination === 'style' ||
+        request.destination === 'script'
+    );
+};
+
+registerRoute(
+    matchCallback,
+    new StaleWhileRevalidate({
+        cacheName,
+        plugins:[
+            new CacheableResponsePlugin({
+                statuses: [0,200],
+            }),
+        ],
+    })
+);
